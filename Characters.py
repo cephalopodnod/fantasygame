7609f73e3563d5d14e3ede968a98f0ebdfd875ae
cephalopodnod <timothycobbs2@gmail.com>
2024-10-10 03:29:51 -0400

    update 10.10.24

--- Characters.py
+++ Characters.py
@@ -17,6 +17,7 @@ class Warrior():
         self.armor_types = ['cloth', 'leather', 'chainmail', 'plate']
         self.skills = [{'Name': 'Slash', 'Damage': 10, 'Effect': '', 'Damage Type': 'Slashing'}]
         self.spells = []
+    
     def add_skill(self, level=0):
         if level in Warrior.SKILL_DICT.keys():
             self.skills.append(Warrior.SKILL_DICT[level])
@@ -40,44 +41,39 @@ class Character:
         self.inventory = []
         self.currency = 0
         self.equipment = {'head': {}, 'chest': {}, 'legs': {}, 'weapon': {}, 'off-hand': {}}
+        # Need to fix this up.
+        self.defense = spec.CON + self.equipment
 
     def level_up(self):
         self.level += 1
         self.spec.add_skill(self.level)
-    def add_item(self,amount,item):
+    
+    def add_item(self,amount=1,item={}):
         self.inventory.append({'Item':item,'Quantity':amount})
 
-class Consumable:
-    def __int__(self,name,stat='',increase=0):
-        self.name = name
-        self.stat = stat
-        self.increase = increase
+    def equip_item(self,position='',item={}):
+        if self.equipment[position] == {}:
+            self.equipment[position] = item
+            self.inventory.remove(item)
+        else:
+            self.inventory.append(self.equipment[position])
+            self.equipment[position] = item
+            self.inventory.remove(item)
+         
+    def attack(self,target):
+        attack = (atk + weapon_atk) - target.defense
+        crit_multiplier = 0.10 * (self.LCK * 0.04)
+        target.hp = target.hp - attack * crit_multiplier
+         
+class Enemy:
+    def __init__(self,hp=0,mp=100,armor=0,mr=0,atk=0) -> None:
+        self.hp = hp
+        self.mp = mp
+        self.armor = armor
+        self.mr = mr
+        self.atk = atk
+        self.abilities = []
 
-class Armor:
-    def __init__(self,position,type,armor=0,mr=0,bonus={}):
-        self.slot = position
-        self.type = type
-        self.stats = {'Armor':armor,'Magic Resist':mr}
-        self.bonus_trait = bonus
-
-class Weapon:
-    def __init__(self,type,hands=1,damage=0,atkspd=0,bonus={}):
-        self.type = type
-        self.hands = hands
-        self.stats = {'Damage':damage,'Attack Speed':atkspd}
-        self.bonus_trait = bonus        
-
-class OffHand:
-    '''
-    brace -> enhance melee 2 hander
-    shield -> 1 hand defensive with 1 hand melee or caster
-    ammo -> enhances ranged weapon
-    bauble -> enhances mage with 1 hand melee or caster
-    off-hand -> enhance melee with 1 hand melee or caster
-    '''
-    OFFHAND_TYPES = ['brace','shield','ammo','bauble','off-hand']
-    
-    def __init__(self,type,stats={},bonus={}):
-        self.type = type
-        self.stats = stats
-        self.bonus_trait = bonus   
+    def attack(self,target):
+        attack = self.atk - target.defense
+        target.hp = target.hp - attack
