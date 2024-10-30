36a508bc3ca1f95d792eb04f11fcf7703b7f276c
cephalopodnod <timothycobbs2@gmail.com>
2024-10-30 18:11:36 -0400

    some story and more functionality

---
+++ TheJester.py
@@ -0,0 +1,25 @@
+'''
+Story:
+    Across your travels, if you are in 
+Events:
+6 pictures: 
+    1) Kings Court, artist unknown, oil painting
+        perception: 1-3, just a picture of a kings court dinner nothing unique
+        perception: 4-7, additionally you see everyone seems to be having a good time
+        perception: 8-10, additionally you notice everyone is smiling, but something feels a bit off but you can't figure out why
+        perception: 11-13, additionally, you notice that while everyone is smiling everyones eyes look sad
+        perception: 14-16, additionally, you notice that king and queens throats are crimson like recently cut
+        perception: 17-19, additionally, you notice the queen is holding a knife in one hand with blood on it, and her other hand seems to be holding a goblet out towards the center of the room
+        perception: 20, additionally, you notice everyones eyes are focused on the jester, also you notice that there seems to be blood dripping from the table and instruments as well. 
+                    Also, your eyes are drawn to the jester and you notice that it appears that he is facing directly at you, and as you move around the room he seems to follow you.
+    2) The Gamble, artist unknown, oil painting
+        perception: 1-3, just a picture of 5 people playing a card game
+        perception: 4-7, additionally you notice that each player has two cards in their hand and their are 3 on the table
+        perception: 8-10, additionally you notice that each player has their other hand on a weapon, some blades and some guns
+        perception: 11-13, additionally you can notice the hands of the two players one player has a 10,9 and the other player has a joker,10. The three cards on the table are 8,7,6.
+        perception: 14-16, additionally you notice the player with the joker has a smurk on his face and his other hand has the hammer pulled back on his gun.
+        perception: 17-19, additionally, you notice that the joker on card seems to be coming out of the card like its crawling out of the card.
+        perception: 20, additionally you notice that each of the players at the table seem to be in a state of decay, and that the joker appears to be staring at you and watching where you are moving. 
+    3) 
+        
+'''
