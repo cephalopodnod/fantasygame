fc9d2d794dbc5b0421653b89021f2ec50868b919
cephalopodnod <timothycobbs2@gmail.com>
2025-01-26 22:27:18 -0500

    update 1.26.25

--- TheJester.py
+++ TheJester.py
@@ -1,8 +1,23 @@
 '''
-Story:
-    Across your travels, if you are in 
+Hidden Story:
+    A long the storys and paths the parties travels there will be pictures in discreet locations of the major Houses in the region.
+    The major houses all have their own unique storylines that will give the party access to the pictures, though these pictures will not be obvious in all occasions.
+    The map of the area looks normal up front but within the map of the region are hidden symbols within each area and even a combination of symbols that join together into a greater symbolism.
+    The Clown/Jester/Joker/Comedian/Trickster(satyr)/ManyFacedGod always is watching, if you anger it. It will attack, party can't kill it, can only get it to run away. It is the puppetmaster
+    that controls what it wants, it mostly leaves the Houses to whatever they want to do, but if a House tries to do something that actively goes against it or causes too much information,
+    it won't hesitate to take retribution.
+
+Houses:
+    The Crown - The Royal Family 
+    The Coin - The Banking Clan 
+    The Chalice - Priests of The White Flame (this is name can change)
+    The Sword - Bounty Hunters Guild
+    The Shield - Farmers Bounty Organization
+    The Scales - The High Courts
+
 Events:
 6 pictures: 
+    The Crown
     1) Kings Court, artist unknown, oil painting
         perception: 1-3, just a picture of a kings court dinner nothing unique
         perception: 4-7, additionally you see everyone seems to be having a good time
@@ -12,6 +27,7 @@ Events:
         perception: 17-19, additionally, you notice the queen is holding a knife in one hand with blood on it, and her other hand seems to be holding a goblet out towards the center of the room
         perception: 20, additionally, you notice everyones eyes are focused on the jester, also you notice that there seems to be blood dripping from the table and instruments as well. 
                     Also, your eyes are drawn to the jester and you notice that it appears that he is facing directly at you, and as you move around the room he seems to follow you.
+    The Coin
     2) The Gamble, artist unknown, oil painting
         perception: 1-3, just a picture of 5 people playing a card game
         perception: 4-7, additionally you notice that each player has two cards in their hand and their are 3 on the table
@@ -20,6 +36,13 @@ Events:
         perception: 14-16, additionally you notice the player with the joker has a smurk on his face and his other hand has the hammer pulled back on his gun.
         perception: 17-19, additionally, you notice that the joker on card seems to be coming out of the card like its crawling out of the card.
         perception: 20, additionally you notice that each of the players at the table seem to be in a state of decay, and that the joker appears to be staring at you and watching where you are moving. 
+    The Chalice
     3) 
+    The Sword
+    4)
+    The Shield
+    5)
+    The Scales
+    6)
         
 '''
