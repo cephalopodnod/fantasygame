0a50c3fa7a83d4ce76ac2f2a8397f552f00bab78
cephalopodnod <timothycobbs2@gmail.com>
2025-03-31 19:50:02 -0700

    adding board game

---
+++ game_2/board.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+playing = True
+while playing:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            playing = False
+pygame.quit()
+    
