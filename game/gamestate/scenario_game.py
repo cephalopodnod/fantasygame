ac663e1c0efb96d96279de3a740323bbb2e3ff34
timothy <timothycobbs2@gmail.com>
2026-02-22 22:35:52 -0800

    Building from the DnD manuals

--- game/gamestate/scenario_game.py
+++
@@ -1,9 +0,0 @@
-class Event:
-    def __init__(self,reward=[]):
-        self.state = True
-        self.reward = []
-        
-    def complete(self,character):
-        self.state = False
-        for item in self.reward:
-            character.inventory.appened(item)
